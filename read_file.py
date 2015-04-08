4bb363849be19f081e539839b0a3554f0a2bc1a8
Lene Preuss <lene.preuss@gmail.com>
2015-04-08 20:09:17 +0200

    added function to read a CSV file in the given format

---
+++ read_file.py
@@ -0,0 +1,19 @@
+__author__ = 'lene'
+
+import csv
+
+def read_file(filename):
+    """
+    reads a file containing CSV of the form "user_id;product_id"
+    :param filename:    name of the file (duh)
+    :return:            a dict of the form { user_id: [product_id, ...], ... }
+    """
+    return read_from_reader(csv.reader(open(filename), delimiter=';'))
+
+def read_from_reader(reader):
+    values = {}
+    for row in reader:
+        values[int(row[0])] = values[int(row[0])] + [int(row[1])] if int(row[0]) in values else [int(row[1])]
+
+    return values
+
--- test_recommender.py
+++ test_recommender.py
@@ -1,4 +1,5 @@
 from compare_sets import jaccard_coefficient, naive_comparison
+from read_file import read_file
 
 __author__ = 'lene'
 
@@ -24,6 +25,12 @@ class TestRecommender(unittest.TestCase):
 
     def test_naive_comparison_different_sizes(self):
         self.assertEqual(naive_comparison([{'a', 'b'}, {'a'}]), [[1., 0.5], [0.5, 1.]])
+
+    def test_read_file(self):
+        csv = read_file('testdata.csv')
+        self.assertIsInstance(csv, dict)
+        self.assertEqual(len(csv), 5)
+        self.assertDictEqual(csv, { 1: [12, 99, 32], 2: [32, 77, 54, 66], 3: [99, 42, 12, 32], 4: [77, 66, 47], 5: [65]})
 
 if __name__ == '__main__':
     unittest.main()
