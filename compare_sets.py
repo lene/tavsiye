463efeb1db019b826457bd3553e5274fbd55ff4b
Lene Preuss <lene.preuss@gmail.com>
2015-04-08 19:27:46 +0200

    tests for non-trivial cases and a little documentation

--- compare_sets.py
+++ compare_sets.py
@@ -4,8 +4,16 @@ from __future__ import print_function
 __author__ = 'lene'
 
 def jaccard_coefficient(set1, set2):
+    """
+    A measure of similarity between two sets. Always between 0 (completely disjunct) and
+    1 (containing the same elements).
+    """
     return len(set1 & set2)/len(set1 | set2)
 
 def naive_comparison(sets):
+    """
+    :param sets:    A list of sets. Jaccard distance is computed between each pair of sets.
+    :return:        Matrix of jaccard distances between each pair of sets in the list.
+    """
     return [ [ jaccard_coefficient(set1, set2) for set2 in sets] for set1 in sets ]
 
--- test_recommender.py
+++ test_recommender.py
@@ -15,5 +15,15 @@ class TestRecommender(unittest.TestCase):
         self.assertEqual(naive_comparison([{'a'}]), [[1.]])
         self.assertEqual(naive_comparison([{'a'}, {'a'}]), [[1., 1.], [1., 1.]])
 
+    def test_naive_comparison_larger(self):
+        self.assertEqual(naive_comparison([{'a', 'b'}, {'b', 'a'}]), [[1., 1.], [1., 1.]])
+        self.assertEqual(
+            naive_comparison([{'a', 'b'}, {'b', 'a'}, {'c', 'd'}]),
+            [[1., 1., 0.], [1., 1., 0], [0., 0., 1.]]
+        )
+
+    def test_naive_comparison_different_sizes(self):
+        self.assertEqual(naive_comparison([{'a', 'b'}, {'a'}]), [[1., 0.5], [0.5, 1.]])
+
 if __name__ == '__main__':
     unittest.main()
