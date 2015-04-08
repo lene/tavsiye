9d156b729385e4aa40b58de5aaa20940bad73e2c
Lene Preuss <lene.preuss@gmail.com>
2015-04-08 19:12:34 +0200

    tests for jaccard distance

---
+++ test_recommender.py
@@ -0,0 +1,15 @@
+from compare_sets import jaccard_coefficient
+
+__author__ = 'lene'
+
+import unittest
+
+class TestRecommender(unittest.TestCase):
+
+    def test_jaccard(self):
+        self.assertEqual(jaccard_coefficient({'a', 'b'}, {'b', 'a'}), 1.)
+        self.assertEqual(jaccard_coefficient({'a', 'b'}, {'c', 'd'}), 0.)
+        self.assertAlmostEqual(jaccard_coefficient({'a', 'b'}, {'a', 'c'}), 1./3.)
+
+if __name__ == '__main__':
+    unittest.main()
