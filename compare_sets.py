006cd630ca2d9221a75f7595eb8b01e182c99a40
Lene Preuss <lene.preuss@gmail.com>
2015-04-09 16:37:32 +0200

    added docstrings.

--- compare_sets.py
+++ compare_sets.py
@@ -24,7 +24,7 @@ def similarity_matrix(sets, similarity = jaccard_coefficient):
     """
     :param sets:        A dict of sets. Similarity is computed between each pair of sets.
     :param similarity:  Function to compute similarity between two sets. Defaults to jaccard_coefficient.
-    :return:            Two-dimensional dict of jaccard distances between each pair of sets, indexed with the user
+    :return:            Two-dimensional dict of jaccard distances between each pair of sets, indexed with the user.
     """
     return {
         user1: {
@@ -33,9 +33,19 @@ def similarity_matrix(sets, similarity = jaccard_coefficient):
     }
 
 def similar_users(user, similarity_matrix, cutoff):
+    """
+    :param user:                The user to find similar users for.
+    :param similarity_matrix:   Matrix containing the similarity in taste to all other users.
+    :param cutoff:              Similarity value above which users are considered similar.
+    :return:                    List of users with similar taste to user.
+    """
     return [ other_user for other_user in similarity_matrix[user]
                         if user != other_user and similarity_matrix[user][other_user] >= cutoff ]
 
 def recommendations(user, sets, similarity_matrix, cutoff):
+    """
+    :param sets:    All users and their liked products.
+    :return:        Recommended products for user based on the products similar users liked.
+    """
     return { item for similar in similar_users(user, similarity_matrix, cutoff)
                   for item in sets[similar] - sets[user] }
