a08858353513e17316bc74e44aac4159cea79ce4
Lene Preuss <lene.preuss@gmail.com>
2015-05-04 17:46:45 +0200

    added stubs for classes/functions to read data and calculate a cost function

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,25 @@
 
 #include "BenchmarkVienna.h"
 
+#include "FileReader.h"
+#include "CostFunction.h"
+
 typedef float ScalarType;
 
 const unsigned MATRIX_SIZE = 400;
 
 int main() {
 
-    BenchmarkVienna<ScalarType> bench;
+//    BenchmarkVienna<ScalarType> bench;
+//    bench.run(MATRIX_SIZE);
 
-    bench.run(MATRIX_SIZE);
+    viennacl::matrix<ScalarType> X = FileReader::X<ScalarType>(std::string(""));
+    viennacl::vector<ScalarType> y = FileReader::y<ScalarType>(std::string(""));
+    viennacl::vector<ScalarType> theta(y.size());
+
+    CostFunction<ScalarType> cost_function(X, y, theta);
+
+    std::cout << "Cost: " << cost_function();
 
     return 0;
 }
