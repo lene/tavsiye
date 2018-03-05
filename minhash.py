69115ae851112dcf2574aacc61e73acc79c1ebca
Lene Preuss <lene.preuss@gmail.com>
2018-03-05 13:19:52 +0100

    cleaned up minhash reference implementation

--- minhash.py
+++ minhash.py
@@ -2,35 +2,25 @@
 import random
 import operator
 
+from typing import List, Tuple
+from typedefs import User, Item
 
-def minhash(users, q, seed=None):
-    """
-    @users : [(user, [item1, item2.....), (user1, [item3, item4...])...]
-    @q : the num of minhashes
-    """
+UserFavoriteList = List[Tuple[User, List[Item]]]  # [(user1, [item1, ...), ...]
+
+
+def minhash(users: UserFavoriteList, length: int, seed: int=None) -> UserFavoriteList:
     random.seed(seed)
-    user_minhashes = []
-    for user, itemList in users:
-        min_seq_items = get_min_seq_item(itemList, q)
-        user_minhashes.append((user, min_seq_items))
-    return user_minhashes
+    return [(user, get_min_seq_items(items, length)) for user, items in users]
 
 
-def get_min_seq_item(items, q):
-    """
-    @items : [item1, item2...]
-    @q : the num of minSeq
-    @return : [item7, item4, ....]
-    """
-    num = len(items)
-    min_seqs = get_min_seq(q, num)
-    return [items[i] for i in min_seqs]
+def get_min_seq_items(items: List[Item], length: int) -> List[Item]:
+    return [items[i] for i in get_min_seq(length, len(items))]
 
 
-def get_min_seq(q, num):
-    min_seqs = []
-    for i in range(q):
-        seqs = [(i, random.randint(1, 10 ** 6)) for i in range(num)]
-        m = min(seqs, key=operator.itemgetter(1))
-        min_seqs.append(m[0])
-    return min_seqs
+def get_min_seq(minhash_length: int, actual_length: int) -> List[int]:
+    return [get_min_random_index(actual_length) for _ in range(minhash_length)]
+
+
+def get_min_random_index(actual_length: int) -> int:
+    seqs = [(i, random.randint(1, 10 ** 6)) for i in range(actual_length)]
+    return min(seqs, key=operator.itemgetter(1))[0]
